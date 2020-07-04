7af864f3a1f19e346aff5bff835b1641078472e0
anywalker <94050531@qq.com>
2020-07-05 00:37:36 +0800

    Initial commit

---
+++ Module0705/src/HelloWorld/SayHello.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+/**
+ * @author 大地行者
+ * @create 2020-07-05 0:29+
+ */
+
+public class SayHello {
+    public static void main(String[] args) {
+        System.out.println("say hello, world Peace");
+    }
+}
